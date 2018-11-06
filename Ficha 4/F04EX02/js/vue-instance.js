5c6e535d86a17eeb4c34ab416c393ff5b5e8f8eb
Eduardo Madeira Silva <eduardomadeirasilva@gmail.com>
2018-11-06 10:20:55 +0000

    6-11-2018

--- Ficha 4/F04EX02/js/vue-instance.js
+++ Ficha 4/F04EX02/js/vue-instance.js
@@ -1,11 +1,22 @@
 const vm = new Vue({
     el: "#intro",
     data: {
-        schools: ["ESMAD", "ISEP", "ESE", "ISCAP", "ESTG", "ESS", "ESHT"]
+        schools: ["ESMAD", "ISEP", "ESE", "ISCAP", "ESTG", "ESS", "ESHT"],
+        char: ""
     },
     methods: {
-        addSchool(school){
+        addSchool(school) {
             this.schools.push(school)
+        },
+        filterSchool(char) {
+            this.char = char
+        }
+    },
+    computed: {
+        filterSchools() {
+            return this.schools.filter(
+                (school) => school.startsWith(this.char)
+            )
         }
     }
 })
